4934405a730b6615b611dc101635f86e9887e987
Aneet Cheema <aneet.cheema@sourcefuse.com>
2020-06-04 16:49:46 +0530

    Init repo, first commit

---
+++ Test_Devops/Test_Devops/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_Devops
+//
+//  Created by Aneet Cheema on 04/06/20.
+//  Copyright © 2020 Aneet Cheema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_Devops/Test_DevopsTests/Test_DevopsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_DevopsTests.swift
+//  Test_DevopsTests
+//
+//  Created by Aneet Cheema on 04/06/20.
+//  Copyright © 2020 Aneet Cheema. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Devops
+
+class Test_DevopsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
